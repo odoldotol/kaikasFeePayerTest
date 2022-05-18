b172d976d4f23aed69ce6b084a2f8bad98cebd89
odoldotol <odoldotol@gmail.com>
2022-05-18 15:16:36 +0900

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import Caver from 'caver-js'
-
-const caver = new Caver('https://api.baobab.klaytn.net:8651/')
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => {
+        <button onClick={()=>{
           
-        }}>
-          토큰전송
-        </button>
+        }}>토큰전송</button>
       </header>
     </div>
   );
